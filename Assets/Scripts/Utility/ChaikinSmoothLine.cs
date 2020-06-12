0307d7d8f7479f4baeec8a64e5c2e8ef8100435e
Antoine <antoinenry@gmail.com>
2020-06-12 20:07:48 +0200

    Chaikin algorithm corrections

--- Assets/Scripts/Utility/ChaikinSmoothLine.cs
+++ Assets/Scripts/Utility/ChaikinSmoothLine.cs
@@ -1,57 +1,84 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class ChaikinSmoothLine
 {
     static public Vector2[] Smoothen(Vector2[] positions, bool loop)
     {
-        List<Vector2> newPositions = new List<Vector2>();
-        for (int i = 0, imax = loop ? positions.Length : positions.Length - 1; i < imax; i++)
+        if (positions == null) return new Vector2[0];
+        if (positions.Length < 3)
         {
-            Vector2[] sub = DivideAngle(positions[i], positions[(i + 1) % imax], positions[(i + 2) % imax]);
-            newPositions.AddRange(sub);
+            Vector2[] newPositions = new Vector2[positions.Length];
+            positions.CopyTo(newPositions, 0);
+            return newPositions;
         }
-        return newPositions.ToArray();
+        else
+        {
+            List<Vector2> newPositions = new List<Vector2>();
+            newPositions.Add(positions[0]);
+
+            for (int i = 0, length = positions.Length, imax = loop ? length : length - 2; i < imax; i++)
+            {
+                Vector2[] sub = DivideAngle(positions[i], positions[(i + 1) % length], positions[(i + 2) % length]);
+                newPositions.AddRange(sub);
+            }
+
+            if (loop == false)
+            {
+                newPositions.Add(positions[positions.Length - 1]);
+            }
+            else
+            {
+                int lastIndex = newPositions.Count - 1;
+                newPositions[0] = newPositions[lastIndex];
+                newPositions.RemoveAt(lastIndex);
+            }
+
+            return newPositions.ToArray();
+        }
     }
 
-    static public Vector2[] Smoothen(Vector2[] positions, bool loop, int numIterations)
+    static public Vector2[] Smoothen(Vector2[] positions, bool loop, float cornerRadius, int numIterations)
     {
-        if (numIterations < 1) return positions;
-
-        Vector2[] newPositions = positions;
-        for (int i = 0; i < numIterations; i++)
-            newPositions = ChaikinSmoothLine.Smoothen(newPositions, loop);
-
-        return newPositions;
-    }
-
-    static public Vector2[] Smoothen(Vector2[] positions, bool loop, float cornerRadius, int numIterations = 1)
-    {
-        if(cornerRadius <= 0f) return ChaikinSmoothLine.Smoothen(positions, loop, numIterations);
-
-        List<Vector2> newPositions = new List<Vector2>();
-        for (int i = 0, imax = loop ? positions.Length : positions.Length - 1; i < imax; i++)
+        if (numIterations == 0 || cornerRadius <= 0f)
         {
-            Vector2 current = positions[i];
-            Vector2 next = positions[(i+1) % imax];
-            float segmentLength = Vector2.Distance(current, next);
-
-            newPositions.Add(current);
-
-            if(segmentLength > 2f * cornerRadius)
-            {
-                Vector2 segmentDirection = (next - current).normalized;
-                newPositions.Add(current + cornerRadius * segmentDirection);
-                newPositions.Add(next - cornerRadius * segmentDirection);
-            }
-            else if (segmentLength > cornerRadius)
-            {
-                newPositions.Add((current + next) / 2f);
-            }
+            Vector2[] newPositions = new Vector2[positions.Length];
+            positions.CopyTo(newPositions, 0);
+            return newPositions;
         }
+        else
+        {
+            List<Vector2> addPositions = new List<Vector2>();
+            for (int i = 0, imax = positions.Length; i < imax; i++)
+            {
+                Vector2 current = positions[i];
+                Vector2 next = positions[(i + 1) % imax];
+                float segmentLength = Vector2.Distance(current, next);
 
-        return ChaikinSmoothLine.Smoothen(newPositions.ToArray(), loop, numIterations);
+                addPositions.Add(current);
+                if (i == imax - 1 && loop == false) break;
+
+                if (segmentLength > 2f * cornerRadius)
+                {
+                    Vector2 segmentDirection = (next - current).normalized;
+                    addPositions.Add(current + cornerRadius * segmentDirection);
+                    addPositions.Add(next - cornerRadius * segmentDirection);
+                }
+                else if (segmentLength > cornerRadius)
+                {
+                    addPositions.Add((current + next) / 2f);
+                }
+            }
+
+            Vector2[] newPositions = new Vector2[addPositions.Count];
+            addPositions.CopyTo(newPositions, 0);
+
+            
+            for (int i = 0; i < numIterations; i++)
+                newPositions = Smoothen(newPositions, loop);                
+
+            return newPositions;
+        }
     }
 
     static public Vector2[] DivideAngle(Vector2 A, Vector2 B, Vector2 C)
